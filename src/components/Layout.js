be216352eb63530d60af6da958bbea0a4f7fcf80
LLwyct <787552171@qq.com>
2021-05-08 14:30:18 +0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,15 +10,20 @@ export default function Layout({children}) {
         margin: 0 auto;
         padding: 0 1em;
     `;
-    let colorMode = (window.localStorage || localStorage).getItem("b0ee67a4b84e9f55");
+
+    React.useEffect(() => {
+        if (window) {
+            let colorMode = localStorage.getItem("b0ee67a4b84e9f55");
+            if (colorMode) {
+                const colorSwitchBtn = document.getElementById("colorswitch");
+                if (colorSwitchBtn) {
+                    colorSwitchBtn.innerText = colorMode === "dark" ? "light" : "dark";
+                }
+                document.body.classList.add(colorMode.toString());
+            }
+        }
+    }, [])
     
-    if (colorMode) {
-    const colorSwitchBtn = document.getElementById("colorswitch");
-    if (colorSwitchBtn) {
-        colorSwitchBtn.innerText = colorMode === "dark" ? "light" : "dark";
-    }
-    document.body.classList.add(colorMode.toString());
-    }
     return (
         <>
             <Header></Header>
